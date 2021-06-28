4073ab05f8d32abeb4ae6edf7d2147b6066ce6bf
abkenov <abkenov.ablan@gmail.com>
2021-06-29 00:31:14 +0600

    finished implementation

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,12 @@
     v-model="words"
   />
   <button @click="splitWords">split</button>
-  <p>{{ words }}</p>
-  <p>{{ splittedWords }}</p>
+  <p>Сколько слов в минуту хочешь читать?</p>
+  <select v-model="speed">
+    <option>60</option>
+    <option>100</option>
+    <option>400</option>
+  </select>
   <button v-if="splittedWords.length" @click="displayWords">start</button>
   <p>{{ currentWord }}</p>
 </template>
@@ -19,22 +23,35 @@ export default {
     return {
       words: '',
       splittedWords: [],
-      currentWord: ''
+      currentWord: '',
+      re: /\s+|\n+|\t+/,
+      speed: 100,
     }
   },
   methods: {
     splitWords() {
-      this.splittedWords = this.words.trim().split(' ')
+      if(this.words.length){
+        this.splittedWords = this.words.trim().split(this.re)
+      }
     },
     
     displayWords() {
-      this.currentWord = this.splittedWords[0]
-      this.splittedWords.splice(0, 1)
-    }
+      this.speed = 1000 / (this.speed / 60)
+
+      if(this.splittedWords.length) {
+        setInterval(() => {
+          this.currentWord = this.splittedWords[0]
+          this.splittedWords.splice(0, 1)
+        }, this.speed)
+      }
+    },
   }
 };
 </script>
 
 <style>
-
+textarea {
+  height: 555px;
+  width: 1016px;
+}
 </style>
